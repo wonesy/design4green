3f3245d24f5f72ad880b5e267d641907f3738657
root <root@vps478164.ovh.net>
2017-11-23 00:56:54 +0100

    db working; beginning search view

--- bismark/dentist/models.py
+++ bismark/dentist/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
 
+import uuid
 from django.db import models
 
-# Create your models here.
+class Dentists(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    gender = models.CharField(max_length=6)
+    address = models.CharField(max_length=128)
+    city = models.CharField(max_length=100)
+    phone = models.CharField(max_length=16)
+    image_hash = models.CharField(max_length=128)
+    hours = models.CharField(max_length=256)
+    speciality = models.CharField(max_length=100)
+
+class Users(models.Model):
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=128)
+
+class KnownDentists(models.Model):
+    user = models.ForeignKey('Users', on_delete=models.PROTECT)
+    dentist = models.ForeignKey('Dentists', on_delete=models.PROTECT)
--- bismark/dentist/views.py
+++ bismark/dentist/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def search(request):
+    # Search database for parameters in the request
+    # return results in the response
+    pass
+
+def results(request):
+    pass
