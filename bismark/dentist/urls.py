6fae805378ae9c388f9cf945f01c9b6b0154d9ef
root <root@vps478164.ovh.net>
2017-11-24 10:49:50 +0100

    user authentication views

--- bismark/dentist/urls.py
+++ bismark/dentist/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
 	url(r'^search/', views.search, name='search'),
 	url(r'^index/', views.index, name='index'),
+	url(r'^create/', views.createAccount, name='create'),
+	url(r'^auth/', views.authenticate, name='authenticate'),
         #url(r'^', views.index, name='main'),
 ]
